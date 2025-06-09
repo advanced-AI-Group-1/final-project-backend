310a5d2d709d4aaf7b507870dfb21b38fbf3b1b6
kimyk0101 <kimyk7131@gmail.com>
2025-06-09 15:49:11 +0900

    feat: 전역 예외 처리 클래스 및 핸들러 구현

---
+++ src/main/java/com/example/finalproject/exception/error/PdfGenerationException.java
@@ -0,0 +1,7 @@
+package com.example.finalproject.exception.error;
+
+public class PdfGenerationException extends RuntimeException {
+    public PdfGenerationException(String message) {
+        super(message);
+    }
+}
