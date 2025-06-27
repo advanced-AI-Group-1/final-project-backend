4b207478543a9b2a65b85da6beda5a90f0746fdc
leegeonmin2 <0101.lgm@gmail.com>
2025-06-27 21:03:07 +0900

    oauth 연결

--- src/main/java/com/example/finalproject/config/SecurityConfig.java
+++ src/main/java/com/example/finalproject/config/SecurityConfig.java
@@ -1,19 +1,53 @@
+//package com.example.finalproject.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig {
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		http
+//			.csrf(csrf -> csrf.disable())
+//			.authorizeHttpRequests(auth -> auth
+//				.anyRequest().permitAll()
+//			);
+//		return http.build();
+//	}
+//}
+
 package com.example.finalproject.config;
 
+import com.example.finalproject.domain.user.handler.OAuth2SuccessHandler;
+import com.example.finalproject.domain.user.service.OAuth2UserServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final OAuth2UserServiceImpl oAuth2UserService;
+	private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())
-			.authorizeHttpRequests(auth -> auth
-				.anyRequest().permitAll()
-			);
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/", "/login", "/oauth2/**").permitAll()
+						.anyRequest().authenticated()
+				)
+				.oauth2Login(oauth2 -> oauth2
+						.userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
+						.successHandler(oAuth2SuccessHandler)
+				);
+
 		return http.build();
 	}
 }
