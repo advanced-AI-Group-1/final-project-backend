b0562ea95e618c964cc01b2af37e1645f6a7ef6f
KeK <127266806+disneydreamworker@users.noreply.github.com>
2025-06-30 04:53:19 +0900

    :sparkles: 보고서 저장/조회 기능 추가

--- src/main/java/com/example/finalproject/domain/report/repository/ReportRepository.java
+++ src/main/java/com/example/finalproject/domain/report/repository/ReportRepository.java
@@ -1,9 +1,12 @@
 package com.example.finalproject.domain.report.repository;
 
 import com.example.finalproject.domain.report.entity.ReportEntity;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
 public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
     Optional<ReportEntity> findByCorpName(String corpName);
